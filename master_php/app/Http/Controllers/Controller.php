7daadc197d0dce963a9e14e817281d7d783c1f05
dengpeng <dengpeng@outlook.com>
2016-04-13 13:14:16 +0800

    add master service

---
+++ master_php/app/Http/Controllers/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    function getTasks()
+    {
+    }
+
+    function getToken()
+    {
+    }
+
+    function saveResults(Request $request)
+    {
+    }
+}
