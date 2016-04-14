dc949787ff45718c6ca5e2e212dcd7b61b99744f
dengpeng <dengpeng@outlook.com>
2016-04-14 14:24:32 +0800

    post result to backend

---
+++ slave_py/save_result.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+results = {
+    u'Price.Buford@yahoo.com': {u'errorCode': 0, u'status': 403,
+                                u'message': u"[invalid.profile.access]. You don't have access to the profile"},
+    u'yHarvey@yahoo.com': {u'errorCode': 0, u'status': 404, u'message': u"Couldn't find member"},
+    u'Douglas.Michaela@Stracke.com': {u'distance': 3, u'publicProfileUrl': u'https://www.linkedin.com/in/robli'}
+}
+payload = json.dumps(results)
+
+r = requests.post('http://localhost:8000/result', data={'result': payload})
+print r.text
