e9d785ac2b4c63479c9298737c724060d5aa35d8
dengpeng <dengpeng@outlook.com>
2016-05-16 08:53:56 +0800

    add slave server status

--- master_php/resources/views/status.blade.php
+++ master_php/resources/views/status.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
     <div class="container">
+        <h3>Cookie status</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="list-group" id="cookie_list">
+                </ul>
+            </div>
+        </div>
+        <h3>Slave status</h3>
         <table class="table table-striped">
             <thead>
             <tr>
@@ -24,6 +32,18 @@
             @endforeach
             </tbody>
         </table>
-
     </div>
+
+@endsection
+
+@section('script')
+    <script>
+        $.get('{{ env('ACCOUNT_SERVER') }}/api/cookie/status', function (res) {
+            var html = '';
+            for (var key in res) {
+                html += '<li class="list-group-item"><span class="badge">' + res[key] + '</span>' + key + '</li>';
+            }
+            $('#cookie_list').append(html);
+        });
+    </script>
 @endsection
